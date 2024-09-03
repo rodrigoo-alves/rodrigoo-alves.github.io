793469eadded1e46fcf41a960d253313679eebdd
Rodrigo Alves <alvesrodrigo762@gmail.com>
2024-09-03 10:28:59 -0300

    creating hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="mb-20 ml-2 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <h1 className="text-2xl mx-2">My Portfolio</h1>
+        <h1 className="text-2xl mx-2">Meu Portifolio</h1>
       </div>
       <div className="m-6 items-center justify-center">
         <ul className="flex flex-row gap-4">
